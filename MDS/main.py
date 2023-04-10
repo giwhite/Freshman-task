cd0622fd0abea0228629dd13b183e4fed1b0d55c
kui253 <2533917115@qq.com>
2023-04-10 22:24:44 +0800

    添加了生成模式这个args

--- MDS/main.py
+++ MDS/main.py
@@ -13,7 +13,7 @@ def main(args):
     tokenizer = get_tokenizer()
   
     train_dataset = cache_and_load(args,tokenizer,'train')
-    #tdataset = cache_and_load(args,tokenizer,'test')
+    tdataset = cache_and_load(args,tokenizer,'test')
    # test_dataset = tdataset[0]
     #test_summary = tdataset[1]
     my_trainer = Trainer(args,train_dataset)
@@ -25,17 +25,18 @@ if __name__ == "__main__":
     parser.add_argument("--task", default='dailymail', required=False, type=str, help="the dataset to load and use")
     parser.add_argument("--data_dir", default="./data", type=str, help="The input data dir")
     parser.add_argument("--model_dir", default='./checkpoints', required=False, type=str, help="Path to save, load model")
+    parser.add_argument("--model_type", default=1, type=int, help="use what generation version")
 
     parser.add_argument("--root_dir", default="../data/cnn_dailymail/", type=str, help="raw data file dir")
-    parser.add_argument("--input_max_len", default=780, type=int, help="the max input len of a doc")
-    parser.add_argument("--decoder_max_len", default=56, type=int, help="the max input len of a doc in decoder")
+    parser.add_argument("--input_max_len", default=1024, type=int, help="the max input len of a doc")
+    parser.add_argument("--decoder_max_len", default=128, type=int, help="the max input len of a doc in decoder")
     parser.add_argument("--dropout_rate", default=0.1, type=int, help="dropout rate ")
     parser.add_argument("--hidden_dim", default=4096, type=int, help="hidden_size of the possiblity_vcb")
 
-    parser.add_argument("--train_batch_size", default=8, type=int, help="training batch size")
+    parser.add_argument("--train_batch_size", default=1, type=int, help="training batch size")
     parser.add_argument("--test_batch_size", default=16, type=int, help="batch size for test")
     parser.add_argument("--epoch_nums", default=10, type=int, help="epoch numbers")
-    parser.add_argument("--learning_rate", default=1e4, type=int, help="learning rate of the training prograss")
+    parser.add_argument("--learning_rate", default=1e-5, type=int, help="learning rate of the training prograss")
 
     parser.add_argument("--do_train", action="store_true", help="Whether to run training.")
     parser.add_argument("--do_eval", action="store_true", help="Whether to run eval on the test set.")
@@ -50,5 +51,5 @@ if __name__ == "__main__":
     parser.add_argument('--save_steps', type=int, default=200, help="Save checkpoint every X updates steps.")
 
     args= parser.parse_args()
-    args.model_name_or_path = './bart-large'
+    args.model_name_or_path = './bart-base'
     main(args)
