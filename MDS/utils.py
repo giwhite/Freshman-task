b8833db58f2b17077ae3cbe49daa0a851c6030fc
kui253 <2533917115@qq.com>
2023-04-11 09:45:59 +0800

    mmr写了一部分

--- MDS/utils.py
+++ MDS/utils.py
@@ -2,6 +2,7 @@
 import logging
 import evaluate
 from transformers import BartConfig,BartTokenizer
+from model import LSTMEncoder
 
 def get_tokenizer():
     return BartTokenizer.from_pretrained("bart-large")
@@ -23,10 +24,19 @@ def comput_metrics(generated_summary,reference_summaries):
         score = rouge.compute(predictions=generated_summary, references=reference_summary)
         all_rouges.append(score)
 
-    return all_rouges
+    return all_rouges#这个返回的每个句子和标准再要的rouge
 
-if __name__ == "__main__":
-    generated_summary = ["The US has reported more than 200,000 new Covid-19 cases and 100,000 hospitalizations."]
+class MyMMR(object):
+    def __init__(self,args) -> None:
+        
+        self.encoder =LSTMEncoder(args.vocab_size,hidden_dim=args.decode_hidden_dim,embedding_dim=args.embedding_dim)
     
-    reference_summaries =[["The US has hit a new record for Covid-19 cases and hospitalizations."]]*4
-    comput_metrics(generated_summary,reference_summaries)
+    def comput(self,all_sentences_ids,sentences_len,artcile_ids):
+        '''
+        句子id，句子长度，文章中的句子部分
+        '''
+        _,encoded_sentences_vec = self.encoder(all_sentences_ids,sentences_len)
+        #这个出来猜测应该是 b * l * dim
+        #l 求平均就是句子向量
+        # 然后通过artcile选出来的句子向量作平均就是doc_vec
+        
